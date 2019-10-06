03f26bd2b7f9bd0ccedb0347ecc1e664ad430a84
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-10-06 17:43:05 +1100

    Update discovery_update.sh

--- discovery_update.sh
+++ discovery_update.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #Discovery Address Update
 
+rm startmanager.sh
+rm startsecond.sh
+rm skywire_update.sh
+
 curl -o startmanager.sh https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/startmanager.sh
 curl -o startsecond.sh https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/startsecond.sh
 curl -o skywire_update.sh https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/skywire_update.sh
 
 
 cd
-chmod 777 startmanager.sh
-chmod 777 startsecond.sh
-chmod 777 skywire_update.sh
+sudo chmod 777 startmanager.sh
+sudo chmod 777 startsecond.sh
+sudo chmod 777 skywire_update.sh
 
 cd
 iplist="/home/pi/ips.txt"
