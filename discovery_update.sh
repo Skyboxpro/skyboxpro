b0556cc74e5333c351ec64808db778b596275fee
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-10-06 17:58:49 +1100

    Update discovery_update.sh

--- discovery_update.sh
+++ discovery_update.sh
@@ -11,11 +11,21 @@ cd
 sudo chmod 777 startmanager.sh
 sudo chmod 777 startsecond.sh
 
+echo ""
+echo "*****  Transferring the new discovery address to all secondary nodes  *****"
+echo "------------------------------------------------------"
+echo ""
+
 cd
 iplist="/home/pi/ips.txt"
 while IFS= read -r ip
 do  
   scp /home/pi/startsecond.sh $ip:/home/pi
 done < "$iplist"
+
+echo ""
+echo "*****  Updating SKywire Software to latest version  *****"
+echo "------------------------------------------------------"
+echo ""
 
 bash <(curl -sL https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/skywire_update.sh)
