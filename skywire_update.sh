c3bc1d53ecb72a24c44d3f48b3397bde2e2f73b4
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-10-06 18:00:19 +1100

    Update skywire_update.sh

--- skywire_update.sh
+++ skywire_update.sh
@@ -2,6 +2,12 @@
 
 # Update Skywire on local node
 
+echo ""
+echo "*****  Updating SKywire Software to latest version  *****"
+echo "------------------------------------------------------"
+echo ""
+
+
 cd /home/pi/go/src/github.com/skycoin/skywire
 git checkout master && git reset --hard && git clean -f -d
 git pull https://github.com/skycoin/skywire.git
