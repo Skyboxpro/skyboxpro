61a7e03c8d9c2a01d792be2df043788dd2983dd4
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-10-10 23:10:39 +1100

    Update skywire_update.sh

--- skywire_update.sh
+++ skywire_update.sh
@@ -11,7 +11,8 @@ echo ""
 cd /home/pi/go/src/github.com/skycoin/skywire
 git checkout master && git reset --hard && git clean -f -d
 git pull https://github.com/skycoin/skywire.git
-cd && go install ./...
+cd $GOPATH/src/github.com/skycoin/skywire/cmd
+go install ./...
 
 echo "Rebooting now..."
 sudo reboot
