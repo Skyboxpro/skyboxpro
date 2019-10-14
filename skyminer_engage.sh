efd0f91e7e2622bae8e740d80c4933cbf90566fe
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-10-14 22:45:48 +1100

    Update skyminer_engage.sh

--- skyminer_engage.sh
+++ skyminer_engage.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 # Get MAC addresses of all Node and write to macs.txt
-./findmacs.sh
+bash <(curl -sL https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/findmacs.sh)
 
 #Get IP addresses of each node and write to ips.txt
-./findip.sh
+bash <(curl -sL https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/findip.sh)
 
 #Set up SSH keys between Manager and each secondary node
-./sshkeys.sh
+bash <(curl -sL https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/sshkeys.sh)
 
 #Send Manager Mac address to all secondary nodes
-./manmac.sh
+bash <(curl -sL https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/manmac.sh)
 
-#Enable startmanager.service and startsecond.service on the Manager and seocndary nodes respectively
-./enableboot.sh
-
-#StartManager Interface and manager node
-sudo systemctl start manager
-
-#Reboot all secondary nodes. Sky wire will start after reboot
-./rebootsecond.sh
+#Reboot all secondary nodes. Skywire will start after reboot
+bash <(curl -sL https://raw.githubusercontent.com/Skyboxpro/skyboxpro/master/rebootsecond.sh)
